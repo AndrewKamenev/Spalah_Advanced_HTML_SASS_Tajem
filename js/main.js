be3bfce4baaa601875468baf43636e891d5061b3
AndrewKamenev <Ancam@yandex.ru>
2016-09-07 00:29:21 +0300

    the end on 060916

--- js/main.js
+++ js/main.js
@@ -1,53 +1,79 @@
-$(document).ready(function(){
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    autoplay: true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
-});
-$('.owl-carousel-review').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    autoplay: true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
-})
-});
-
-//   $('.owl-carousel').owlCarousel({
+// $(document).ready(function(){
+// $('.owl-carousel').owlCarousel({
 //     loop:true,
 //     margin:10,
 //     nav:true,
+//     autoplay: true,
 //     responsive:{
 //         0:{
 //             items:1
 //         },
 //         600:{
-//             items:3
+//             items:1
 //         },
 //         1000:{
-//             items:5
+//             items:1
 //         }
 //     }
 // });
+// $('.owl-carousel-review').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     autoplay: true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:1
+//         },
+//         1000:{
+//             items:1
+//         }
+//     }
+// })
+// });
+$(document).ready(function() {
+ 	  	$(".owl-carousel").owlCarousel({
+	 
+	      loop:true,
+		    margin:10,
+		    nav:true,
+		    autoplay: true,
+		    
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        600:{
+		            items:1
+		        },
+		        1000:{
+		            items:1
+		        }
+		    }
+	 	  });	
+
+
+
+ 	  	$(".owl-carousel-review").owlCarousel({
+			// loop:true,
+		 //    margin:10,
+		    nav:true,
+		    // autoplay: true,
+		     responsive:{
+		        0:{
+		            items:1
+		        },
+		        600:{
+		            items:1
+		        },
+		        1000:{
+		            items:1
+		        }
+		    }
+	 	  });	
+	 	  $('.owl-prev, .owl-next').html('');	  	
+		});
+		
